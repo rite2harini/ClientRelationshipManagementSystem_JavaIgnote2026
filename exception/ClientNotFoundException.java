1aa0ff36de7e9e53f5d95991b9d2b773647ff48d
rite2harini <rite2harini@gmail.com>
2026-06-07 19:05:44 +0530

    Update ClientNotFoundException.java

--- exception/ClientNotFoundException.java
+++ exception/ClientNotFoundException.java
@@ -1,8 +1,13 @@
 package exception;
-
 /*
- * Throw when client does not exist.
- */
+Create a custom exception.
+
+Throw this exception when:
+- Client ID is not found during search.
+- Client ID is not found during update.
+- Client ID is not found during delete.
+
+*/
 public class ClientNotFoundException {
 
 }
