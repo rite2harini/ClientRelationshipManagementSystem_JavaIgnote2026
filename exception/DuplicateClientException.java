e5b62efa7f50bd8abc3c504e1b63d07de71a3980
rite2harini <rite2harini@gmail.com>
2026-06-07 19:06:09 +0530

    Update DuplicateClientException.java

--- exception/DuplicateClientException.java
+++ exception/DuplicateClientException.java
@@ -1,8 +1,10 @@
 package exception;
-
 /*
- * Throw when duplicate client ID is entered.
- */
+Create a custom exception.
+
+Throw this exception when:
+- A client is added with an ID that already exists.
+*/
 public class DuplicateClientException {
 
 }
