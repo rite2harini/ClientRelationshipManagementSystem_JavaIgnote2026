d92ffef3ecb31a514901225a500120fccf9bb077
rite2harini <rite2harini@gmail.com>
2026-06-07 19:02:42 +0530

    Update Client.java

--- model/Client.java
+++ model/Client.java
@@ -1,20 +1,27 @@
 package model;
-
 /*
- * Base Client class.
- *
- * Attributes:
- * clientId
- * clientName
- * email
- * phone
- * city
- *
- * Tasks:
- * - Create constructors
- * - Create getters/setters
- * - Override toString()
- */
+PROBLEM REQUIREMENT:
+
+Create a Client class to store client information.
+
+Attributes:
+
+clientId
+clientName
+email
+phone
+city
+
+Tasks:
+
+Create private instance variables.
+Create a parameterized constructor.
+Create getters and setters.
+Override toString() to display client details.
+
+HINT:
+A client object should represent one customer of the company.
+*/
 public class Client {
 
 }
