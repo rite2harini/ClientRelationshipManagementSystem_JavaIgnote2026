767e95f89bbe01a2243f364c115f08321da58e16
rite2harini <rite2harini@gmail.com>
2026-06-07 18:20:43 +0530

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,1 +1,15 @@
+package main;
 
+/*
+ * Entry point of the application.
+ *
+ * Responsibilities:
+ * 1. Display menu
+ * 2. Accept user choice
+ * 3. Call service methods
+ * 4. Handle exceptions
+ * 5. Exit application
+ */
+public class Main {
+
+}
