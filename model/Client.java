a8fa142a3b811478fde37054404acb2bdc6c4673
rite2harini <rite2harini@gmail.com>
2026-06-07 18:21:46 +0530

    Update Client.java

--- model/Client.java
+++ model/Client.java
@@ -1,1 +1,20 @@
+package model;
 
+/*
+ * Base Client class.
+ *
+ * Attributes:
+ * clientId
+ * clientName
+ * email
+ * phone
+ * city
+ *
+ * Tasks:
+ * - Create constructors
+ * - Create getters/setters
+ * - Override toString()
+ */
+public class Client {
+
+}
